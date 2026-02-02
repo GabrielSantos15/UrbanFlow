99136731e748750527e983b683080b5105f02d54
Gabriel dos Santos <gs303291@gmail.com>
2026-02-02 18:38:06 -0300

    feat: sessão Concept no about

--- src/components/about/Concept.jsx
+++ src/components/about/Concept.jsx
@@ -1,19 +1,24 @@
-import skateManobra from "../../assets/images/skateManobra.jpg"
+import skateManobra from "../../assets/images/skateManobra.jpg";
 import styles from "./Concept.module.css";
 
 export default function Concept() {
   return (
     <section className={styles.conceptSection}>
-      <figure>
-        <img src={skateManobra} alt="Skatista no ar realizando uma manobra em uma pista de skate" />
-      </figure>
       <article>
-        <h2>UrbanFlow é movimento</h2>
+        <h2>
+          UrbanFlow é movimento
+        </h2>
         <p>
           UrbanFlow nasce da união entre cultura urbana, skate e arte de rua,
           traduzindo esse universo em uma experiência digital moderna
         </p>
       </article>
+      <figure>
+        <img
+          src={skateManobra}
+          alt="Skatista no ar realizando uma manobra em uma pista de skate"
+        />
+      </figure>
     </section>
   );
 }
